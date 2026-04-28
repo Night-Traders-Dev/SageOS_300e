2902106d3902525c1efb53c3e41517a238ab7529
Night-Traders-Dev <quegmeister@gmail.com>
2026-04-28 19:43:14 -0400

    fix: update qemu_run to use -display option for curses instead of -curses

--- lenovo_300e.sh
+++ lenovo_300e.sh
@@ -309,7 +309,7 @@ qemu_run() {
       -cpu Skylake-Client \
       -drive id=hd0,file="$IMG",format=raw,media=disk,snapshot=on \
       -m 256M \
-      -curses \
+      -display curses \
       -monitor none \
       -device isa-debug-exit,iobase=0x501,iosize=2; then
         local rc=$?

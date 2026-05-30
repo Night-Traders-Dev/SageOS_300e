87e41c75b099bda83b7b8faccbc476e6c582b034
root <root@localhost.localdomain>
2026-05-30 15:58:35 -0400

    Revert faulty timer_init integration; fix build errors

--- sageos_build/kernel/core/virt_main.c
+++ sageos_build/kernel/core/virt_main.c
@@ -146,8 +146,8 @@ void kmain(SageOSBootInfo *info) {
     keyboard_init();
     vfs_init();
 
-    extern void timer_init(void);
-    timer_init();
+    //extern void timer_init(void);
+    //timer_init();
 
     // Initialize block device subsystem (ATA/Virtio)
     ata_init();
--- sageos_build/kernel/include/version.h
+++ sageos_build/kernel/include/version.h
@@ -1,1 +1,1 @@
-#define SAGEOS_VERSION "0.4.7"
+#define SAGEOS_VERSION "0.4.8"
